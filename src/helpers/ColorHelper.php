0f1593fb72c0776bfcd49643a5b8e694379a8c9a
Ben Callaway <ben@findarace.com>
2021-07-23 17:58:21 +0100

    UI Fixes

--- src/helpers/ColorHelper.php
+++ src/helpers/ColorHelper.php
@@ -19,12 +19,12 @@ class ColorHelper
             'white' => [
                 'label' => Craft::t('colorit', 'White'),
                 'handle' => 'white',
-                'color' => '#FFFFFF'
+                'color' => 'FFFFFF'
             ],
             'black' => [
                 'label' => Craft::t('colorit', 'Black'),
                 'handle' => 'black',
-                'color' => '#000000'
+                'color' => '000000'
             ],
         ];
 
@@ -107,7 +107,7 @@ class ColorHelper
 
     public static function isValidHex($color)
     {
-        return preg_match('/^#[0-9a-f]{3}(?:[0-9a-f]{3})?$/i', $color) ? true : false;
+        return preg_match('/^#?[0-9a-f]{3}(?:[0-9a-f]{3})?$/i', $color) ? true : false;
     }
 
     public static function hexToRgba($color, $opacity = false, $asArray = false)
